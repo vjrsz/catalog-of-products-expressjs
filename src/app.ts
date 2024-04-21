f7cca7d2dea85f9c4748197f7f32ad74d77f97e6
vjrsz <juniornunes1008@gmail.com>
2024-04-21 15:35:14 -0300

    update readme

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,6 @@ import logger from "@infrastructure/utils/logger";
 import express from "express";
 import config from "@/config/config";
 import routes from "@/routes/routes";
-import UserData from "@infrastructure/databases/providers/sequelize/models/userData";
 
 const port = config.port
 const app = express()
@@ -14,6 +13,3 @@ routes.mount(app)
 app.listen(port, () => {
     logger.info(`App loaded in port ${port} [${config.env}]`);
 })
-
-UserData.findAll().then(res => console.log(res))
-
